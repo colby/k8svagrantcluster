b7fcc7073fb9d71ee1a6243dd4c95e547d3444f3
Colby Olson <me@colbyolson.com>
2019-02-24 15:10:46 -0800

    remove dumb script, use CLI command and user config

--- cookbooks/keepalived/recipes/default.rb
+++ cookbooks/keepalived/recipes/default.rb
@@ -8,12 +8,7 @@ apt_package node['keepalived']['packages']
 template '/etc/keepalived/keepalived.conf' do
   source 'keepalived.conf.erb'
   mode '0644'
-  notifies :restart, 'service[keepalived]', :immediately
-end
-
-file '/etc/keepalived/check_kubernetes' do
-  content '</dev/tcp/127.0.0.1/6443'
-  mode '0755'
+  notifies :restart, 'service[keepalived]', :delayed
 end
 
 service 'keepalived' do
