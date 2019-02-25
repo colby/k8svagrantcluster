876ea5094fb5cf57eb6dc641d97bfc0684e6bc3d
Colby Olson <me@colbyolson.com>
2019-02-25 09:39:20 -0800

    fix for the fix of the fix

--- cookbooks/kubernetes/recipes/configure.rb
+++ cookbooks/kubernetes/recipes/configure.rb
@@ -3,18 +3,21 @@
 # Recipe:: configure
 #
 
+enp0s8 = node['network']['interfaces']['enp0s8']['addresses'].keys[1]
+
 # NOTE: this is an issue between vagrant and kubernetes
 # kubelet wants to use enp0s3, we want to use enp0s8
 # https://github.com/kubernetes/kubernetes/issues/44702
 file '/etc/default/kubelet' do
-  content "KUBELET_EXTRA_ARGS=--node-ip=#{node['network']['interfaces']['enp0s8']['addresses'].keys[1]}"
+  content "KUBELET_EXTRA_ARGS=--node-ip=#{enp0s8}"
   notifies :restart, 'service[kubelet]', :delayed
 end
 
 # NOTE: this is *still* the issue from above, but now using yaml with kubeadm disregards $KUBELET_EXTRA_ARGS
-execute 'resolve hostname to correct address' do
-  command "sudo sed -i 's/127.0.0.1\t#{node['hostname']}/127.0.0.2\#{node['hostname']}/' /etc/hosts"
+execute 'resolve hostname to vagrant private address' do
+  command "sed -i 's/127.0.1.1/#{enp0s8}/' /etc/hosts"
   not_if 'grep 10.10.3 /etc/hosts'
+  notifies :restart, 'service[kubelet]', :delayed
 end
 
 group node['kubernetes']['group']
@@ -37,6 +40,7 @@ execute 'turn off swap' do
   command 'swapoff -a'
   # NOTE: test if command returns any swap files currently on
   only_if 'test -n "$(swapon --noheadings)"'
+  notifies :restart, 'service[kubelet]', :delayed
 end
 
 execute 'disable swap on boot' do
